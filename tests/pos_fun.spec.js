573e3a41456800a5b3f44fba0dd975bdf5ce164e
hiruwa2003 <dilshanhirusha093@gmail.com>
2026-01-31 12:10:33 +0530

    Add function tests from 1 to 5 in pos_fun.spec.js

---
+++ tests/pos_fun.spec.js
@@ -0,0 +1,51 @@
+import { test, expect } from "@playwright/test";
+
+const URL = "https://www.swifttranslator.com/";
+
+const getSinglishInput = (page) =>
+  page.getByPlaceholder("Input Your Singlish Text Here.");
+
+const getSinhalaOutput = (page) =>
+  page
+    .locator("div.panel-title", { hasText: "Sinhala" })
+    .locator("..")
+    .locator("div")
+    .filter({ hasNotText: "Sinhala" })
+    .first();
+
+const fillAndExpect = async (page, input, expectedText) => {
+  await page.goto(URL);
+  await getSinglishInput(page).fill(input);
+  const output = getSinhalaOutput(page);
+  if (expectedText) {
+    await expect(output).toContainText(expectedText);
+  } else {
+    await expect(output).toBeVisible();
+  }
+};
+
+test.describe("Positive Functional Tests (24) - Singlish to Sinhala", () => {
+  test("Pos_Fun_0001 - Simple sentence", async ({ page }) => {
+    await fillAndExpect(page, "mama gedhara yanavaa", "මම");
+  });
+});
+
+test("Pos_Fun_0002 - Simple request", async ({ page }) => {
+  await fillAndExpect(page, "mata bath oonee", "මට");
+});
+
+test("Pos_Fun_0003 - Simple plural", async ({ page }) => {
+  await fillAndExpect(page, "api paasal yanavaa", "අපි");
+});
+
+test("Pos_Fun_0004 - Compound sentence", async ({ page }) => {
+  await fillAndExpect(
+    page,
+    "api kaema kanna yanavaa saha passe chithrapatayak balanavaa",
+    "සහ",
+  );
+});
+
+test("Pos_Fun_0005 - Compound agreement", async ({ page }) => {
+  await fillAndExpect(page, "oyaa hari ehenam api yamu", "ඔයා");
+});
